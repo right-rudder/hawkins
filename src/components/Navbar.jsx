9c6f5fa611af5aa9d40dba79a238b9ec583c5c80
lucascr <lcrodrigues1995@gmail.com>
2025-06-23 15:40:37 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = ({ pathname }) => {
     <nav >
       <div
         className={`${
-          navBar || openMobile ? "bg-transparent backdrop-blur-lg rounded-sm max-w-7xl mx-auto" : "bg-transparent"
+          navBar || openMobile ? "bg-white/30 backdrop-blur-lg" : "bg-transparent"
         } duration-500`}
       >
         <div className="px-5 max-w-7xl mx-auto top-0">
@@ -92,7 +92,8 @@ const Navbar = ({ pathname }) => {
                 className="relative hover:brightness-110 duration-200 ease-in-out w-1/3"
               >
                 <img
-                  src="/hawkins-flight-academy-logo-shelbyville-tennessee-tupelo-mississippi-flight-school-1.svg"
+                  // src="/hawkins-flight-academy-logo-shelbyville-tennessee-tupelo-mississippi-flight-school-1.svg"
+                  src={navBar || openMobile ? "/hawkins-flight-academy-logo-shelbyville-tennessee-tupelo-mississippi-flight-school-1.svg" : "/hawkins-flight-academy-logo-shelbyville-tennessee-tupelo-mississippi-flight-school-Untitled-design-2.svg"}
                   alt="Hawkins Flight Academy Logo"
                   aria-label="Hawkins Flight Academy Logo"
                   title="Hawkins Flight Academy"
@@ -102,7 +103,7 @@ const Navbar = ({ pathname }) => {
               </a>
               <div className="hidden lg:flex w-full text-2xl">
                 {/* <ul className="flex justify-start align-middle w-11/12 gap-5 items-center"> */}
-                <ul className={`${navBar || openMobile ? "flex justify-around items-center" : "flex justify-start text-lg items-end"} align-middle w-11/12 gap-5 `}>
+                <ul className={`${navBar || openMobile ? "flex justify-around items-center" : "flex justify-start text-lg items-center"} align-middle w-11/12 gap-5 `}>
                   {navbarLinks.map((item, index) => (
                     <li
                       key={index}
@@ -114,26 +115,26 @@ const Navbar = ({ pathname }) => {
                         <a
                           href={item.link}
                           target={`${item.link.includes("http") ? "_blank" : "_self"}`}
-                          className="group-last:block  font-semibold duration-300 hover:underline decoration-accent-300 decoration-4 underline-offset-[10px] group-last:italic border-accent whitespace-nowrap group-last:font-normal group-last:bg-white group-last:px-0 group-last:rounded-sm group-last:-skew-x-12 group-last:hover:bg-accent-500 group-last:hover:no-underline"
+                          className="group-last:block  font-semibold duration-300 hover:underline decoration-accent-300 decoration-4 underline-offset-[10px] group-last:italic border-accent whitespace-nowrap group-last:font-normal group-last:bg-white group-last:px-0 group-last:rounded-sm group-last:-skew-x-12 group-last:hover:bg-accent-900 group-last:hover:no-underline"
                         >
-                          {/* <span className="relative font-medium block italic group-last:skew-x-12 text-blue-900 group-last:text-muted-950 group-last:py-3 group-last:px-5 group-last:hover:text-white"> */}
-                          <span className={`${navBar || openMobile ? "group-last:py-3 group-last:px-5" : "group-last:px-2 pt-6"} relative font-medium block italic group-last:skew-x-12 text-blue-900 group-last:text-muted-950 group-last:hover:text-white`}>
+                          {/* <span className="relative font-medium block italic group-last:skew-x-12 text-accent-900 group-last:text-muted-950 group-last:py-3 group-last:px-5 group-last:hover:text-white"> */}
+                          <span className={`${navBar || openMobile ? "group-last:py-3 group-last:px-5" : "group-last:px-2"} relative font-medium block italic group-last:skew-x-12 text-accent-900 group-last:text-muted-950 group-last:hover:text-white`}>
                             {item.name}
                           </span>
                         </a>
                       ) : (
-                        <span className="font-medium cursor-default text-blue-900 text-2xl duration-300 hover:underline decoration-accent-300 decoration-4 underline-offset-[10px] py-12 whitespace-nowrap">
+                        <span className="font-medium cursor-default text-accent-900 text-2xl duration-300 hover:underline decoration-accent-300 decoration-4 underline-offset-[10px] py-12 whitespace-nowrap">
                           {item.name}
                         </span>
                       )}
                       {item.submenu && item.submenu.length > 0 && (
                         <ul
-                          className={`absolute top-16 bg-white rounded-sm border-b-4 border-white whitespace-nowrap text-primary-950 -left-4 duration-200 ease-out ${hoveredIndex === index ? "max-h-auto w-auto opacity-100" : "max-h-0 h-0 opacity-0 overflow-hidden"}`}
+                          className={`absolute top-16 bg-white rounded-sm border-b-4 border-white whitespace-nowrap text-accent-900 -left-4 duration-200 ease-out ${hoveredIndex === index ? "max-h-auto w-auto opacity-100" : "max-h-0 h-0 opacity-0 overflow-hidden"}`}
                         >
                           {item.submenu.map((subitem, subIndex) => (
                             <li
                               key={subIndex}
-                              className={`${isActive(subitem, pathname) ? "scale-110 bg-accent-600 text-blue-900" : ""} relative rounded-sm -skew-x-12 hover:bg-accent-600 hover:scale-110 duration-200 px-3 hover:font-medium hover:shadow-sm drop-shadow-sm font-medium hover:text-white`}
+                              className={`${isActive(subitem, pathname) ? "scale-110 bg-accent-950 text-accent-900" : ""} relative rounded-sm -skew-x-12 hover:bg-accent-950 hover:scale-110 duration-200 px-3 hover:font-medium hover:shadow-sm drop-shadow-sm font-medium hover:text-white`}
                               onMouseEnter={() => setSubHoveredIndex(subIndex)}
                               onMouseLeave={() => setSubHoveredIndex(null)}
                             >
@@ -196,7 +197,7 @@ const Navbar = ({ pathname }) => {
                 <span className="sr-only">Open main menu</span>
 
                 <svg
-                  className={`${openMobile ? "hidden" : "block"} h-6 w-6 text-blue-900`}
+                  className={`${openMobile ? "hidden" : "block"} h-6 w-6 text-accent-900`}
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="2.0"
@@ -212,7 +213,7 @@ const Navbar = ({ pathname }) => {
                 </svg>
 
                 <svg
-                  className={`${openMobile ? "block" : "hidden"} h-6 w-6 text-blue-900`}
+                  className={`${openMobile ? "block" : "hidden"} h-6 w-6 text-accent-900`}
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="2.0"
@@ -289,7 +290,7 @@ const Navbar = ({ pathname }) => {
                 <a
                   href={item.link}
                   target={`${item.link.includes("http") ? "_blank" : "_self"}`}
-                  className="font-normal text-3xl block p-5 duration-300 text-blue-900 border-accent-200 whitespace-nowrap group-last:bg-accent-600 group-last:font-medium group-last:border-2 group-last:mt-5 group-last:py-4 group-last:px-8 group-last:-skew-x-12 group-last:text-center group-last:mx-5"
+                  className="font-normal text-3xl block p-5 duration-300 text-accent-900 border-accent-200 whitespace-nowrap group-last:bg-accent-950 group-last:font-medium group-last:border-2 group-last:mt-5 group-last:py-4 group-last:px-8 group-last:-skew-x-12 group-last:text-center group-last:mx-5"
                 >
                   {item.name}
                 </a>
@@ -301,7 +302,7 @@ const Navbar = ({ pathname }) => {
                     `}
                   >
                     <IoIosArrowForward
-                      className={`${hoveredIndex === index ? "text-accent-600" : "text-accent-600"} size-5`}
+                      className={`${hoveredIndex === index ? "text-accent-950" : "text-accent-950"} size-5`}
                     />
                   </div>
                 </div>
@@ -370,7 +371,7 @@ const Navbar = ({ pathname }) => {
               href={`tel:${PHONE_NUMBER}`}
               className="border p-2 w-fit border-accent-200 rounded-full bg-black"
             >
-              <FaPhone className="size-4 text-blue-900" />
+              <FaPhone className="size-4 text-accent-900" />
             </a>
             <a aria-labelledby="Call Us Now" title="Call Us Now" href={`tel:${PHONE_NUMBER}`}>{PHONE_NUMBER}</a>
           </div>
