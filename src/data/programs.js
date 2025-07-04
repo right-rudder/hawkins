6eaa3f922c1ef4607de0ba72fb38c691469a5017
lucascr <lcrodrigues1995@gmail.com>
2025-07-04 18:50:31 -0300

    Update programs.js

--- src/data/programs.js
+++ src/data/programs.js
@@ -1,9 +1,13 @@
 import privatePilot from "./privatePilot";
 import instrumentRating from "./instrumentRating";
+import commercialPilot from "./commercialPilot";
+import multiEngine from "./multiEngine";
 
 const programs = [
   privatePilot,
   instrumentRating,
+  commercialPilot,
+  multiEngine,
 ];
 
 export default programs;
